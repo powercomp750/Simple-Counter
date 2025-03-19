9570ac813c1b40648f03ea89f9f8e64411ee312a
powercomp750 <developeraryan.750@gmail.com>
2025-03-19 19:03:49 +0530

    Add new color definitions and theme management for light and dark modes

--- app/src/main/java/comp/basic/simplecounter/ui/theme/Color.kt
+++ app/src/main/java/comp/basic/simplecounter/ui/theme/Color.kt
@@ -9,3 +9,33 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val OrangeBold = Color(0xFFF14A00)
+val OrangeDark = Color(0xFFFF5722)
+val OrangeLight = Color(0xFFE07B39)
+
+val WhiteClean = Color(0xFFFFFFFF)
+val Darkboard = Color(0xFF2F2D2D)
+
+sealed class ThemeColors(
+    val background: Color,
+    val surface: Color,
+    val primary: Color,
+    val text: Color,
+    val secondary: Color
+)  {
+    object Night: ThemeColors(
+        background = Darkboard,
+        surface = Darkboard,
+        primary = OrangeDark,
+        text = Color.White,
+        secondary = Color.Gray
+    )
+    object Day: ThemeColors(
+        background = WhiteClean,
+        surface = WhiteClean,
+        primary = OrangeBold,
+        secondary = OrangeLight,
+        text = Color.Black
+    )
+}
